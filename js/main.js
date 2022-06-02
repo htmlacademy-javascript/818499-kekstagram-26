cccb036c113596aecbdb92184717f162432eff28
Malefikar <pavel721@mail.ru>
2022-06-03 01:55:52 +0700

    Второе дз

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sringLenght(str) {
+  if (str.length < 140){
+    return sringLenght;
+  }
+}
