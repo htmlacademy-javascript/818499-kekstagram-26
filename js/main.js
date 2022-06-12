4b95089b3f629d3f2b2b18b410727a8117880b8f
Malefikar <pavel721@mail.ru>
2022-06-12 13:07:16 +0700

    правки по дз

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
 const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
     throw new Error('Число не должно быть меньше нуля');
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-console.log(getRandomInt(0,15));
-
 
 const getLenght = (string, lengthNumber = 140) => {
   if (typeof string !== 'string' || typeof lengthNumber !== 'number') {
