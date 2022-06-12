eb2633dfc16a872681861dd71998ffba3768b229
Malefikar <pavel721@mail.ru>
2022-06-12 13:04:22 +0700

    правки по дз

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
-function sringLenght(str) {
-  if (str.length < 140){
-    return sringLenght;
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    throw new Error('Число не должно быть меньше нуля');
   }
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log(getRandomInt(0,15));
+
+
+const getLenght = (string, lengthNumber = 140) => {
+  if (typeof string !== 'string' || typeof lengthNumber !== 'number') {
+    throw new Error('Строка не подходит по типу данных');
+  }
+  return string.length <= lengthNumber;
+};
