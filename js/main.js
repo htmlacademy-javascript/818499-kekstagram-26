0b3804f9c52cce50f145594dd4f81fb2dcfd633a
Malefikar <pavel721@mail.ru>
2022-06-12 13:24:08 +0700

    правки по дз2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
     throw new Error('Число не должно быть меньше нуля');
   }
+  if (min > max) {
+    return Math.floor(Math.random() * (max - min - 1) + 1 + min);
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+console.log(getRandomInt(5, 0));
 
-const getLenght = (string, lengthNumber = 140) => {
-  if (typeof string !== 'string' || typeof lengthNumber !== 'number') {
-    throw new Error('Строка не подходит по типу данных');
-  }
-  return string.length <= lengthNumber;
-};
+const getLenght = (string, lengthNumber = 140) => string.length <= lengthNumber;
