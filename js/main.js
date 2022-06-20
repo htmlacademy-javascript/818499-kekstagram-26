c0c9f42b261f0a43b3e8250199a050c8cd149bb6
Malefikar <pavel721@mail.ru>
2022-06-20 18:03:35 +0700

    Вариант 2

--- js/main.js
+++ js/main.js
@@ -14,32 +14,31 @@ getRandomInt (1, 5);
 getLenght('test');
 
 
-const ID = [];
-const URL = [];
-const DESCRIPTION = ['Вид на пляж', 'Указатель', 'Машина в городе', 'Краб', 'Пальмы вечером'];
-const LIKES = [];
-const COMMENTS = [
-  idCommtent = [],
-  AVATAR = [],
-  MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'],
-  NAME = ['Иван', 'Петр', 'Алексанр', 'Елена', 'Мария', 'Олег']
-];
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomInt(0, elements.length - 1)];
-};
+const ID = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+const DESCRIPTION = ['Вид на пляж', 'Указатель', 'Машина в городе', 'Краб', 'Пальмы вечером'];
+const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+const NAME = ['Иван', 'Петр', 'Алексанр', 'Елена', 'Мария', 'Олег'];
+const URL = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
 const createObject = () => ({
-    id: getRandomInt(1, 25),
-    description: getRandomArrayElement(DESCRIPTION),
-    likes: getRandomInt(15, 200),
-    comments: {
-      id: (Math.floor(Math.random() * 100) + 1),
-      message: getRandomArrayElement(MESSAGE),
-      name: getRandomArrayElement(NAME),
-    }
-  });
+  id: ID.pop(),
+  url: `photos/${URL.pop()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomInt(15, 200),
+  comments: [
+    idComment = getRandom(1000),
+    messageComment = getRandomArrayElement(MESSAGE),
+    nameComment = getRandomArrayElement(NAME),
+    avatarComment = `img/avatar-${getRandom(3)}.svg`,
+  ]
+});
 
-
-console.log(createObject());
+const photos = Array.from({length: 25}, createObject);
+console.log(photos);
 createObject();
