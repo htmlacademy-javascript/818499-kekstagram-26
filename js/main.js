ca2a92e4fa2e1277d04dd9fb283638ff34628182
Malefikar <pavel721@mail.ru>
2022-06-20 12:37:17 +0700

    первый вариант

--- js/main.js
+++ js/main.js
@@ -12,3 +12,34 @@ const getLenght = (string, lengthNumber = 140) => string.length <= lengthNumber;
 
 getRandomInt (1, 5);
 getLenght('test');
+
+
+const ID = [];
+const URL = [];
+const DESCRIPTION = ['Вид на пляж', 'Указатель', 'Машина в городе', 'Краб', 'Пальмы вечером'];
+const LIKES = [];
+const COMMENTS = [
+  idCommtent = [],
+  AVATAR = [],
+  MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'],
+  NAME = ['Иван', 'Петр', 'Алексанр', 'Елена', 'Мария', 'Олег']
+];
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInt(0, elements.length - 1)];
+};
+
+const createObject = () => ({
+    id: getRandomInt(1, 25),
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomInt(15, 200),
+    comments: {
+      id: (Math.floor(Math.random() * 100) + 1),
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAME),
+    }
+  });
+
+
+console.log(createObject());
+createObject();
